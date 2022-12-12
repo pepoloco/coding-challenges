e9a2c8fabc6d99273173679377539ccff432c51e
Pepo Banchev <banchevpepo@gmail.com>
2022-12-12 22:09:47 +0200

    no message

---
+++ 04-codingChallenge.js
@@ -0,0 +1,7 @@
+function codingChallenge() {
+    const bill = 275;
+    const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+    console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${tip + bill}`);
+}
+codingChallenge();
+
