eb5462aa8e00fc8e2ce38aee9c429c6aa8cb1041
Pepo Banchev <banchevpepo@gmail.com>
2023-01-03 00:06:08 +0200

    no message

---
+++ 06-codingChallenge.js
@@ -0,0 +1,12 @@
+function calcTip() {
+    const bill = [125, 555, 44];
+    const randomElement = bill[Math.floor(Math.random() * bill.length)];
+    const calculate = randomElement => randomElement >= 30 && randomElement <= 300 ? randomElement * 0.15 : randomElement * 0.20;
+    const tip = calculate(randomElement);
+    const total = calculate(randomElement) + randomElement;
+
+    console.log(total);
+
+}
+calcTip()
+//Test data: 125, 555 and 44
