9c7a86db8b1a315b2a1dab01a519b17013b43a08
Pepo Banchev <banchevpepo@gmail.com>
2023-01-03 00:06:15 +0200

    no message

--- 04-codingChallenge.js
+++ 04-codingChallenge.js
@@ -1,7 +1,6 @@
-function codingChallenge() {
-    const bill = 275;
-    const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
-    console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${tip + bill}`);
-}
-codingChallenge();
-
+const calculate = bill => bill >= 30 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+const bill = [275, 40, 430];
+const tip = [calculate(bill[0]), calculate(bill[1]), calculate(bill[2])];
+const total = [bill[0] + tip[0], bill[1] + tip[1], bill[2] + tip[2]];
+console.log(total);
+//Data 1: Test for bill values 275, 40 and 430
